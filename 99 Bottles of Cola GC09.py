1e960283893213eb34f84b1de0d2b66db8b60dd7
paopee <62095007+paopee@users.noreply.github.com>
2020-03-18 23:22:33 +0800

    Add files via upload

---
+++ 99 Bottles of Cola GC09.py
@@ -0,0 +1,7 @@
+import time
+count = 99
+for i in range(99):
+    print(count,"bottles of cola on the wall.",count,"bottles of cola, take one down, pass it around")
+    count =  count -1
+    time.sleep(1)
+

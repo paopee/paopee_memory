7832acefe8e28c9e7279847d33dad267e69c5b45
paopee <62095007+paopee@users.noreply.github.com>
2020-03-20 20:45:28 +0800

    Add files via upload

---
+++ Demo algorithms GC05.py
@@ -0,0 +1,28 @@
+#sequence
+mystring = "abcdef"
+
+print (mystring)
+print (mystring [0:3])
+
+
+# selection
+
+day ="monday"
+
+if day =="monday":
+    print("Correct")
+else:
+    print("error")
+
+
+
+#repetition
+
+for i in range(3):
+    print("hi")
+
+
+
+#variable
+
+day = input("Day:")

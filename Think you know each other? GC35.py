66922d1ac162f818dd833f3ed021d0be607db37e
paopee <62095007+paopee@users.noreply.github.com>
2020-03-20 18:00:10 +0800

    Add files via upload

---
+++ Think you know each other? GC35.py
@@ -0,0 +1,11 @@
+question = input("Input a question")
+ans = input ("Input an Answer")
+
+print("Past the computer to the other user")
+
+print("Question:", question)
+ans_2 = input("Input your answer:")
+if ans_2 != ans:
+    print("Wrong")
+else:
+    print("Correct")
